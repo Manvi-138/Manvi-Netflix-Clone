9a867534c8f322b26b2c87009a85004acc5133ab
Manvi-138 <manvibhardwaj097@gmail.com>
2023-11-07 00:09:15 +0530

    last

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           <Route
             path='/account'
             element={
-              <ProtectedRoute>
+              <ProtectedRoute> 
                 <Account />
               </ProtectedRoute>
             }
